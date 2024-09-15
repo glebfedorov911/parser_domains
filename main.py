5359d3b12ce9db4cbec807ea2a0e7a0e4ba56cfb
gfedorov911 <fedorov22134@gmail.com>
2024-09-15 13:21:20 +0300

    delete func src and ip, edit func phone, edit find urls, add shablons, change unique parser, add open domains

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ def data_from_file(filename: str) -> list:
         reader = csv.reader(file, delimiter=' ', quotechar="|")
         return [row[0].split(";")[:-1] for row in list(reader)[1:] if row[0].split(";")[:-1][0] == "2024-07-15"]
 
-def get_src(html: str, url: str):
-    split_comma = html.split("\"")
-    return [http for data in split_comma for http in data.split(" ") if "http" in http and http not in (url, url[:-1]) if "http" in data and "://" in data]
-
 def find_email(html: str):
     return re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}(?=\s|$|>|<)", html)
 
 def find_phone(html: str):
-    return re.findall(r"\+[0-9]{9,15}(?=\s|$|>|<)", html) + re.findall(r"\+\d{1,3} \(\d{3}\) \d{3}-\d{2}-\d{2}", html) + re.findall(r"8 \(\d{3}\) \d{3}-\d{2}-\d{2}", html) + re.findall(r"8\d{10}", html)
+    pat1 = r"(?<=[\s><:])\+[0-9]{9,15}(?=\s|$|>|<)"
+    pat2 = r"(?<=[\s><:])\+\d{1,3} \(\d{3}\) \d{3}-\d{2}-\d{2}"
+    pat3 = r"(?<=[\s><:])8 \(\d{3}\) \d{3}-\d{2}-\d{2}"
+    pat4 = r"(?<=[\s><:])8\d{10}"
+    return re.findall(pat1, html) + re.findall(pat2, html) + re.findall(pat3, html) + re.findall(pat4, html)
 
 def find_inn(html: str):
     return re.findall(r"ИНН [a-zA-Z0-9.-]{12}(?=\s|$|>|<)", html)
@@ -36,16 +36,24 @@ def find_ooo(html: str):
 def find_individual(html: str):
     return re.findall(r"ИП [a-zA-Zа-яА-Я0-9.-]{1,50} [a-zA-Zа-яА-Я0-9.-]{1,50} [a-zA-Zа-яА-Я0-9.-]{1,50}(?=\s|$|>|<)", html)
 
-def find_ip(html: str):
-    return re.findall(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(?=\s|$|>|<)", html)
-
 def find_urls(html: str, url: str):
     soup = BeautifulSoup(html, "html.parser")
     hrefs = [a.get("href") for a in soup.find_all("a", href=True)]
+    final_hrefs = []
     for idx in range(len(hrefs)):
-        if "http" not in hrefs[idx]:
-            hrefs[idx] = "http://" + url + hrefs[idx]
-    return hrefs
+        if any([True if exp in hrefs[idx] else False for exp in ("javascript", "js", "src", "io", "png", "jpg", "svg", "webp")]):
+            continue
+        elif not "http" in hrefs[idx]:
+            if hrefs[idx] != '':
+                if url[-1] == hrefs[idx][0] == "/":
+                    final_hrefs.append(url + hrefs[idx][1:])
+                else:
+                    final_hrefs.append(url + hrefs[idx])
+            else:
+                final_hrefs.append(url + hrefs[idx])
+        else:
+            final_hrefs.append(url)
+    return list(set(final_hrefs))
 
 
 # def find_domain(html: str):
@@ -75,6 +83,18 @@ SHABLON = [
     """,
     """
     <p class="lead">This page is used to test the proper operation of the <a href="http://apache.org">Apache HTTP server</a> after it has been installed. If you can read this page it means that this site is working properly.
+    """,
+    """
+  <g class="ds-site-logo__icon">
+    <path d="M26.3618 10.5399H17.6244L14.7667 7.68225C14.0077 6.92323 12.9829 6.5 11.909 6.5H4.08105V10.5399H11.9125L14.7702 13.3976C15.5292 14.1566 16.5541 14.5799 17.6279 14.5799H25.4594V28.0148H29.4993V13.6774C29.4993 11.946 28.0967 10.5399 26.3618 10.5399Z" fill="#3755FA"></path>
+    <path d="M4.08093 10.5398H0.0410156V28.9171C0.0410156 30.6485 1.44362 32.0546 3.17851 32.0546H25.4592V28.0147H4.08093V10.5398Z" fill="#3755FA"></path>
+  </g>
+    """,
+        """
+  <g class="ds-site-logo__icon">
+    <path d="M26.3618 10.5399H17.6244L14.7667 7.68225C14.0077 6.92323 12.9829 6.5 11.909 6.5H4.08105V10.5399H11.9125L14.7702 13.3976C15.5292 14.1566 16.5541 14.5799 17.6279 14.5799H25.4594V28.0148H29.4993V13.6774C29.4993 11.946 28.0967 10.5399 26.3618 10.5399Z" fill="#3755FA" />
+    <path d="M4.08093 10.5398H0.0410156V28.9171C0.0410156 30.6485 1.44362 32.0546 3.17851 32.0546H25.4592V28.0147H4.08093V10.5398Z" fill="#3755FA" />
+  </g>
     """
 ]
 
@@ -98,10 +118,18 @@ DELETE = [
 #     except requests.exceptions.ConnectionError:
 #         print("exp")
 
+def unique(data: list, src: list):
+    data[src[1]]["email"] = list(set(data[src[1]]["email"]))
+    data[src[1]]["phone"] = list(set(data[src[1]]["phone"]))
+    data[src[1]]["inn"] = list(set(data[src[1]]["inn"]))
+    data[src[1]]["ooo"] = list(set(data[src[1]]["ooo"]))
+    data[src[1]]["individual"] = list(set(data[src[1]]["individual"]))
+    del data[src[1]]["domain"]
+
 def parser(all_data: list):
-    global data, bad, good, check_again
+    global data, bad, good, check_again, total
     for src in all_data:
-        print(src[1])
+        print("not under", src[1])
         try:
             req = requests.get("http://" + src[1], timeout=5)
         except requests.exceptions.ConnectionError:
@@ -123,14 +151,35 @@ def parser(all_data: list):
             continue
 
         data[src[1]] = {}
-        data[src[1]]["email"] = " ".join(find_email(req.text))
-        data[src[1]]["phone"] = " ".join(find_phone(req.text))
-        data[src[1]]["inn"] = " ".join(find_inn(req.text))
-        data[src[1]]["ooo"] = " ".join(find_ooo(req.text))
-        data[src[1]]["individual"] = " ".join(find_individual(req.text))
-        data[src[1]]["ip"] = " ".join(find_ip(req.text))
-        data[src[1]]["domain"] = " ".join(find_urls(req.text, req.url))
+        data[src[1]]["email"] = find_email(req.text)
+        data[src[1]]["phone"] = find_phone(req.text)
+        data[src[1]]["inn"] = find_inn(req.text)
+        data[src[1]]["ooo"] = find_ooo(req.text)
+        data[src[1]]["individual"] = find_individual(req.text)
+        data[src[1]]["domain"] = find_urls(req.text, req.url)
         good += 1
+        for under_src in data[src[1]]["domain"]:
+            try: #переписать в потоки
+                print("under", under_src)
+                under_req = requests.get(under_src, timeout=3)
+                data[src[1]]["email"] += find_email(under_req.text)
+                data[src[1]]["phone"] += find_phone(under_req.text)
+                data[src[1]]["inn"] += find_inn(under_req.text)
+                data[src[1]]["ooo"] += find_ooo(under_req.text)
+                data[src[1]]["individual"] += find_individual(under_req.text)
+            except requests.exceptions.MissingSchema:
+                continue
+            except requests.exceptions.ConnectionError:
+                continue
+            except requests.exceptions.Timeout:
+                continue
+            except requests.exceptions.TooManyRedirects:
+                continue
+            except requests.exceptions.InvalidSchema:
+                continue
+        unique(data, src)
+        total += 1
+        print(total, "/ 2361")
 
 def split_file(nums: int, data: list):
     start = 0
@@ -145,35 +194,36 @@ def split_file(nums: int, data: list):
         end = start + len(data) // nums
     return new_data
 
-# data = {}
-# bad = good = check_again = 0
-# filename = "ihead_domains_1725961813_4457.csv"
-# all_data = data_from_file(filename)
+data = {}
+total = 0
+bad = good = check_again = 0
+filename = "ihead_domains_1725961813_4457.csv"
+all_data = data_from_file(filename)
 
-# thrs = []
-# for num in range(16):
-#     thr = threading.Thread(target=parser, args=(split_file(16, all_data)[num], ))
-#     thr.start()
-#     thrs.append(thr)
+# with open("test1.txt", "w", encoding="UTF-8") as file:
+#     req = requests.get("http://GIGASTOR.ru")
+#     file.write(str(req.text))
+
+thrs = []
+for num in range(16):
+    thr = threading.Thread(target=parser, args=(split_file(16, all_data)[num], ))
+    thr.start()
+    thrs.append(thr)
     
-# for thr in thrs:
-#     thr.join()
+for thr in thrs:
+    thr.join()
 
-# print("-=-=-=-=-=-=")
-# print(data)
-# with open("test.txt", "w", encoding="UTF-8") as file:
-#     file.write(str(data))
-# print("-=-=-=-=-=-=")
+print("-=-=-=-=-=-=")
+print(data)
+with open("test.txt", "w", encoding="UTF-8") as file:
+    file.write(str(data))
+print("-=-=-=-=-=-=")
 
-# print("-=-=-=-=-=-=")
-# print("good:", good)
-# print("bad:", bad)
-# print("check again:", check_again)
-# print("-=-=-=-=-=-=")
-
-req = requests.get("http://MOSTLINGSIBOWRARA.ru")
-with open('test1.txt', "w", encoding="UTF-8") as file:
-    file.write(req.text)
+print("-=-=-=-=-=-=")
+print("good:", good)
+print("bad:", bad)
+print("check again:", check_again)
+print("-=-=-=-=-=-=")
 
 # test = "big@desktop +79869466585 fedorov22134@gmail.com ИНН 012345678912 ООО ПАРАМ ПАРАМ ИП ПАРАМ ПАРАМ ПАРАМ ПАРААМ 25.255.25.1 aaaa.ru"
 
