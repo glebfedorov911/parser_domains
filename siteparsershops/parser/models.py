7ff42cf947d3d495e4eced1c3981aeae6a675f89
gfedorov911 <fedorov22134@gmail.com>
2024-09-17 16:01:43 +0300

    add tables and migration django

--- siteparsershops/parser/models.py
+++ siteparsershops/parser/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class DateModel(models.Model):
+    date = models.CharField(max_length=21)
+
+class AgainShablonModel(models.Model):
+    code = models.TextField(null=False)
+
+class DeleteShablonModel(models.Model):
+    code = models.TextField(null=False)
+
+class FileModel(models.Model):
+    file = models.FileField(upload_to="uploads/%Y/%m/%d/")
+
+class StatisticsModel(models.Model):
+    good = models.IntegerField(null=False)
+    bad = models.IntegerField(null=False)
+    check_again = models.IntegerField(null=False)
+    file = models.OneToOneField(FileModel, null=True, on_delete=models.SET_NULL)
+
+class ShowDataModel(models.Model):
+    domain = models.TextField(null=False)
+    phone = models.CharField(max_length=20, null=True)
+    email = models.EmailField(null=True)
+    inn = models.CharField(max_length=15, null=True)
+    ooo = models.TextField(null=True)
+    ip = models.TextField(null=True)
+    file = models.OneToOneField(FileModel, null=True, on_delete=models.SET_NULL)
+
+class AgainDataModel(models.Model):
+    domain = models.TextField(null=False)
