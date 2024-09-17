1b72e238694ee0379182c2a317c880784c9c0453
gfedorov911 <fedorov22134@gmail.com>
2024-09-17 16:23:24 +0300

    add adminpanel models

--- siteparsershops/parser/admin.py
+++ siteparsershops/parser/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(DateModel)
+admin.site.register(AgainShablonModel)
+admin.site.register(DeleteShablonModel)
+admin.site.register(FileModel)
+admin.site.register(StatisticsModel)
+admin.site.register(ShowDataModel)
+admin.site.register(AgainDataModel)
