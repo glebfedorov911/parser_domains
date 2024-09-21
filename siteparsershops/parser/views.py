66332e0b87fd72e7b15221b397643778c4e25e36
gfedorov911 <fedorov22134@gmail.com>
2024-09-21 17:16:17 +0300

    add print

--- siteparsershops/parser/views.py
+++ siteparsershops/parser/views.py
@@ -75,13 +75,14 @@ class ParserView(TemplateView):
     def get(self, request):
         if (self.request.GET.get('start') == "True" or self.request.GET.get('again') == "True") and not self._is_start_parser:
             # try:
+            print('1')
             if self.request.GET.get('again') == "True":
                 files = AgainDataModel.objects.all()
                 all_data = [eval(file.domain) for file in files]
             else:
                 files = FileModel.objects.all().order_by("-id")
                 all_data = data_from_file(f"{MEDIA_ROOT}/{files[0].file}")
-
+            print('2')
             delete = [delete.code for delete in DeleteShablonModel.objects.all()]
             shablon = [again.code for again in AgainShablonModel.objects.all()]
 
@@ -89,23 +90,23 @@ class ParserView(TemplateView):
                 all_data = split_file(len(all_data) if len(all_data) != 0 else 1, all_data)
             else:
                 all_data = split_file(16, all_data)
-
+            print('3')
             res = self.start_parser(all_data, delete, shablon)
             good = bad = check_again = 0
             again_domain = []
             data = []
-
+            print('4')
             if self.request.GET.get('again') == "True":
                 files = AgainDataModel.objects.all().delete()
                 files = FileModel.objects.all().order_by("-id")
-
+            print('5')
             for r in res:
                 again_domain += r["again_domain"]
                 good += r["good"]
                 check_again += r["check_again"]
                 bad += r["bad"]
                 data += [(i, r["data"][i]) for i in r["data"]]
-            
+            print('6')
             StatisticsModel.objects.create(good=good, bad=bad, check_again=check_again, file=files[0]).save()
             for row in data:
                 if any([row[1][r] != [] for r in row[1]]):
@@ -114,13 +115,13 @@ class ParserView(TemplateView):
                 else:
                     ShowDataModel.objects.create(domain=row[0], phone=', '.join(row[1]["phone"]), email=', '.join(row[1]["email"]), inn=', '.join(row[1]["inn"]),
                                                 ooo=', '.join(row[1]["ooo"]), ip=', '.join(row[1]["individual"]), is_showing=False, file=files[0]).save()
-
+            print('7')
             for row in again_domain:
                 AgainDataModel.objects.create(domain=row)
-            
+            print('8')
             self._is_start_parser = False   
             url_redirect = self.get_url()
-
+            print('9')
             return HttpResponseRedirect(url_redirect)
             # except:
             #     return HttpResponse("""<h1>Файл не загружен либо загружен неправильно</h1> <br> <a href='/parser'>Вернуться на главную страницу</a>""")
