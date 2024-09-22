270bf9ef53e1c26a260870c74a9cd0a598d74f29
gfedorov911 <fedorov22134@gmail.com>
2024-09-22 14:39:47 +0300

    edit db

--- siteparsershops/siteparsershops/settings.py
+++ siteparsershops/siteparsershops/settings.py
@@ -88,13 +88,12 @@ ASGI_APPLICATION = 'siteparsershops.asgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
-        "HOST": os.getenv("HOST"),
-        "PORT": os.getenv("PORT"),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
 
