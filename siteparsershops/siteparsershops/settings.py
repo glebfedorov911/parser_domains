03add0cd0211a1544a40bdb12242034bb6db9813
gfedorov911 <fedorov22134@gmail.com>
2024-09-22 20:30:41 +0300

    edit settings

--- siteparsershops/siteparsershops/settings.py
+++ siteparsershops/siteparsershops/settings.py
@@ -30,15 +30,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = [
-#     "domains-main-31a38f-5007f5-82-146-32-206.traefik.me",
-# ]
+ALLOWED_HOSTS = [
+    "domains-main-31a38f-5007f5-82-146-32-206.traefik.me",
+]
 
-# CSRF_TRUSTED_ORIGINS  = [
-#     "https://domains-main-31a38f-5007f5-82-146-32-206.traefik.me",
-# ]
+CSRF_TRUSTED_ORIGINS  = [
+    "https://domains-main-31a38f-5007f5-82-146-32-206.traefik.me",
+]
 
 # Application definition
 
