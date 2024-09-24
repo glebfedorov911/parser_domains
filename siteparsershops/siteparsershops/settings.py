372b115b0e209284d25e1508705c1109f1866490
VYACHESLAV204 <125289802+VYACHESLAV204@users.noreply.github.com>
2024-09-24 19:07:40 +0300

    commit

--- siteparsershops/siteparsershops/settings.py
+++ siteparsershops/siteparsershops/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "domains-main-31a38f-f51009-82-146-32-206.traefik.me",
