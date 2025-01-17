b8dcbfb6a90c8a9c7a644595ff50a1e8888fb9c6
Gleb <fedorov22134@gmail.com>
2025-01-17 18:59:41 +0300

    edit

--- siteparsershops/siteparsershops/urls.py
+++ siteparsershops/siteparsershops/urls.py
@@ -32,3 +32,7 @@ handler404 = page_not_found
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:  
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
