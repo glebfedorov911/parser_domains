beac6ef268d84751ddf10d91594f59236f03d10c
Gleb <fedorov22134@gmail.com>
2025-01-18 12:59:08 +0300

    test3

--- siteparsershops/parser/views.py
+++ siteparsershops/parser/views.py
@@ -17,6 +17,7 @@ import re
 import threading
 import json
 import multiprocessing
+import os
 
 
 #uvicorn siteparsershops.asgi:application --host 127.0.0.1 --port 8000
@@ -294,9 +295,12 @@ def delete_dublicate(request):
     return HttpResponse('Good')
 
 def test_work_with_db(request):
+    if os.path.exists('siteparsershops\parser\data.txt'):
+        os.remove('siteparsershops\parser\data.txt')
     with open(r'siteparsershops\parser\data.txt', 'a') as file:
         for i in UploadDataModel.objects.all()[:500]:
             tabulation_data = f"{i.date}\t{i.domain_for_parsing}\t{i.domain}\t{i.phone}\t{i.email}\t{i.inn}\t{i.ooo}\t{i.ip}\t{i.is_check}\t{i.is_showing}\t{i.it_was_good}\t{i.status}\t{i.status_good}\t"
             file.write(tabulation_data)
             file.write('\n')
+    print("готово")
     return HttpResponse("kaif")
