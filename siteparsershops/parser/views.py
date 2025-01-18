475329b852ae73258d5fb037748899aa47c42748
Gleb <fedorov22134@gmail.com>
2025-01-18 13:04:36 +0300

    test7

--- siteparsershops/parser/views.py
+++ siteparsershops/parser/views.py
@@ -295,15 +295,13 @@ def delete_dublicate(request):
     return HttpResponse('Good')
 
 def test_work_with_db(request):
-    if os.path.exists(r'siteparsershops\parser\data.txt'):
-        os.remove(r'siteparsershops\parser\data.txt')
-    # if os.path.exists(r'data.txt'):
-        # os.remove(r'data.txt')
-    # with open(r'data.txt', 'a') as file:
-    #     for i in UploadDataModel.objects.all()[:500]:
-    #         tabulation_data = f"{i.date}\t{i.domain_for_parsing}\t{i.domain}\t{i.phone}\t{i.email}\t{i.inn}\t{i.ooo}\t{i.ip}\t{i.is_check}\t{i.is_showing}\t{i.it_was_good}\t{i.status}\t{i.status_good}\t"
-    #         file.write(tabulation_data)
-    #         file.write('\n')
+    if os.path.exists(r'data.txt'):
+        os.remove(r'data.txt')
+    with open(r'data.txt', 'a') as file:
+        for i in UploadDataModel.objects.all()[:500]:
+            tabulation_data = f"{i.date}\t{i.domain_for_parsing}\t{i.domain}\t{i.phone}\t{i.email}\t{i.inn}\t{i.ooo}\t{i.ip}\t{i.is_check}\t{i.is_showing}\t{i.it_was_good}\t{i.status}\t{i.status_good}\t"
+            file.write(tabulation_data)
+            file.write('\n')
     print("готово")
     return HttpResponse("kaif")
 
